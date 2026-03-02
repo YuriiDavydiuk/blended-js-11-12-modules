58d0f15bf16666cf32f85e6c9401128ca4992e6c
YuriiDavydiuk <davydiuk.yurii@gmail.com>
2026-03-02 14:15:46 +0000

    Deploying to gh-pages from @ YuriiDavydiuk/blended-js-11-12-modules@37b6123f647329e0bf8209a7b823e1a1fafc2bbc 🚀

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-JE8YjOlG.js";import{a as t}from"./assets/vendor-Dl2X3eg5.js";t.defaults.baseURL="https://dummyjson.com/docs/products";async function o(){const{data:s}=await t.get("/category-list");return["ALL",...s]}o();
+//# sourceMappingURL=index.js.map
